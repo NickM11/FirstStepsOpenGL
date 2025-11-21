812e0afeaac94000e0eb1ec12dd1abf0d594d655
NickM11 <43708706+NickM11@users.noreply.github.com>
2025-11-21 21:54:56 +0100

    Created a square

--- main.cpp
+++ main.cpp
@@ -35,10 +35,17 @@ int main() {
 	//Vertices coordinates
 	GLfloat vertices[] =
 	{
-		-0.5f,-0.5f * float(sqrt(3)) / 3, 0.0f,//Lower left corner
-		0.5f,-0.5f * float(sqrt(3)) / 3, 0.0f,//Lower right corner
-		0.0f,0.5f * float(sqrt(3)) *2 / 3, 0.0f//Upper corner
+		-0.5f /*x*/,-0.5f/*y*/,0.0, // Lower left corner
+		-0.5f,0.5f,0.0, //Upper left corner
+		0.5f,0.5f,0.0, //Upper right corner
+		0.5f,-0.5f,0.0 // Lower right corner
+		
+	};
 
+	GLuint indices[] =
+	{
+		0,1,2, // Lower left triangle
+		2,3,0 // Lower right triangle
 	};
 
 	//Create a GLFWwindow object with entered dimensions
@@ -90,11 +97,12 @@ int main() {
 
 
 	//Creating reference containers for Vertex Array and Vertex Buffers objects
-	GLuint VAO,VBO;
+	GLuint VAO,VBO,EBO;
 	
 	//Generate the VAO and VBO with 1 object each
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1,&EBO);
 
 	//Making the VAO the current Vertex Array Object by binding it
 	glBindVertexArray(VAO);
@@ -103,6 +111,11 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	//Introducing the Vertices into VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
 	//Configure the Vertex Attribute so that OpenGL know how to read VBO
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	//Enable the Vertex Attribute so that OpenGL knows to use it
@@ -111,6 +124,7 @@ int main() {
 	//Bind both VBO and VAO to zero so we don't accidently modify the VAO and change it
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
 	//Specify the background color
@@ -129,7 +143,8 @@ int main() {
 		// Bind VAO so OpenGL knows to use it
 		glBindVertexArray(VAO);
 		// Draw the Triangle using OpenGl Primitive
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		 glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
 		glfwSwapBuffers(window);
 
 		//Take care of all GLFW events
@@ -139,6 +154,7 @@ int main() {
 	//Delete all objects we created
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	//Delete window before ending the program
